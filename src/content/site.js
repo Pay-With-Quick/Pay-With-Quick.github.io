4eb68668583de133061671837d3c1cd2a29d4150
Pranav Kapoor <pranavkapoorr@gmail.com>
2026-03-13 04:05:58 +0000

    redesign

---
+++ src/content/site.js
@@ -0,0 +1,15 @@
+export const site = {
+  name: "Quick",
+  domain: "paywithquick.co.uk",
+  email: "info@paywithquick.co.uk",
+  phone: "+44 7786 088188",
+  address: "Ingeborg, Middle Hill, Egham, England, TW20 0JG"
+};
+
+export const navLinks = [
+  { label: "Who we are", href: "/who-we-are/" },
+  { label: "Our Mission", href: "/our-mission/" },
+  { label: "Why Quick", href: "/why-merchants-choose-quick/" },
+  { label: "Let's Talk Cost", href: "/our-costs/" },
+  { label: "Contacts", href: "/contacts/" }
+];
